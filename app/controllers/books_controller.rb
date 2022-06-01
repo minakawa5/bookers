f4bfa1b9c082b0e0693e8481d0acc19132f84d2b
SoraMinakawa <104938290+minakawa5@users.noreply.github.com>
2022-06-01 14:34:39 +0000

    投稿機能作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,31 @@
 class BooksController < ApplicationController
   def new
+   @book = Book.new
   end
-
+   
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+ 
   def index
+  
   end
-
+  
   def show
   end
-
+  
   def edit
   end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
