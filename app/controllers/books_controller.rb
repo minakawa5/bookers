42be6d52250114c97a6aaec9f2d08f477c922de8
SoraMinakawa <104938290+minakawa5@users.noreply.github.com>
2022-06-04 15:17:43 +0000

    レイアウト調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,23 +3,42 @@ class BooksController < ApplicationController
    @book = Book.new
   end
    
-  def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+   def create
+    @book = Book.new(list_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end  
   end
  
   def index
-  
+   @book = Book.new
+   
+   @books = Book.all
   end
   
   def show
+   @book = Book.find(params[:id])    
   end
   
   def edit
+   @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+   if @book.update(list_params)
+    redirect_to book_path(@book.id)
+   else
+    render :new
+    end
+   end
+  
+   def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books/show'
-  get 'books/edit'
-  get '/top' => 'homes#top'
-  get '/books' => 'books#index'
+ get 'top' => 'homes#top'
+  resources :books
+  
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
